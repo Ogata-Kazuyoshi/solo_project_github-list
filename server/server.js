520705e1281fe128d57888dc57e911bb99d9bd43
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2023-11-21 13:09:59 +0900

    FIX : corsは開発時に必要なため、デプロイ時はコメントアウト

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 // expressのセットアップ
 const express = require('express');
 const apiRoute = require('./Routes');
-const cors = require('cors');
+// const cors = require('cors');
 
 const setupServer = () => {
   const app = express();
-  app.use(
-    cors({
-      origin: 'http://localhost:5173',
-    })
-  );
+  // app.use(
+  //   cors({
+  //     origin: 'http://localhost:5173',
+  //   })
+  // );
 
   app.use(express.json());
   app.use(express.static('./dist/'));
@@ -19,9 +19,3 @@ const setupServer = () => {
 };
 
 module.exports = { setupServer };
-
-// sampleデータベースのbooksテーブルから全てのデータを取得
-
-// app.listen(8080, () => {
-//   console.log(`サーバーが立ち上がりました: http://localhost:8080`);
-// });
